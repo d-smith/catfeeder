8403d5777bc40c13c5a8f812eb10b08bb3d37d49
d-smith <doug.smith.mail@gmail.com>
2018-07-30 06:23:16 -0700

    Updated instructions

--- mySkill.js
+++ mySkill.js
@@ -1,9 +1,15 @@
-const day = () => {
 
+//Server time is assumed to be UTC
+const pacificDate = () => {
   let date = new Date();
   let localOffset = 420 * 60000; //Hmmmm does pacific time always have the same offset from utc?
   let localTime = date.getTime();
-  let theDay = new Date(localTime - localOffset).getDay();
+  return new Date(localTime - localOffset)
+}
+
+const day = () => {
+  let date = pacificDate();
+  let theDay = date.getDay();
 
   switch(theDay) {
     case 0:
@@ -31,17 +37,29 @@ const day = () => {
   }
 }
 
+const dayPeriod = () => {
+  hours = pacificDate().getHours();
+  if(hours < 12) {
+    return "morning";
+  } else if(hours >= 12 && hours < 17) {
+    return "afternoon";
+  } else {
+    return "evening";
+  }
+}
+
 
 module.exports.handler = (event, context, callback) => {
 
-  var theDay = day()
+  let theDay = day()
+  let dayPart = dayPeriod()
 
   const response = {
     version: '1.0',
     response: {
       outputSpeech: {
         type: 'PlainText',
-        text: `Let them eat ${theDay}'s cake`,
+        text: `Feed them from ${theDay}'s ${dayPart} portion.`,
 //        text: 'swa swa swa swa swa swa swa swa swa swa swa swa swa swa swa swa swa swa swa swa swa swa swa swa'
       },
       shouldEndSession: false,
